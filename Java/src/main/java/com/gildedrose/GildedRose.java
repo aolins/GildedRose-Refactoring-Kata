59800b19df746e8bd4c9f1bc8a7909164f4aa610
aolins <andrejs.olins@gmail.com>
2025-03-02 01:55:18 +0200

    working if else if else

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,16 +7,34 @@ class GildedRose {
         this.items = items;
     }
 
+
+    private static void updateConjured(Item item){
+        if (item.name.startsWith("Conjured")){
+            if (item.sellIn > 0){
+                item.quality = item.quality - 2;
+            } else {
+                item.quality = item.quality - 4;
+            }
+            item.sellIn = item.sellIn - 1;
+            updateGlobal(item);
+        }
+    }
+
+    private static void updateGlobal(Item item){
+        if (item.quality < 0){
+            item.quality = 0;
+        }else if (item.quality > 50){
+            item.quality = 50;
+        }
+    }
+
     public void updateQuality() {
         for (Item item: items) {
 
+
+
             if (item.name.startsWith("Conjured")){
-                if (item.sellIn > 0){
-                    item.quality = item.quality - 2;
-                } else {
-                    item.quality = item.quality - 4;
-                }
-                item.sellIn = item.sellIn - 1;
+                updateConjured(item);
            } else if (item.name.startsWith("Aged Brie")) {
                 if (item.sellIn > 0){
                     item.quality = item.quality + 1;
